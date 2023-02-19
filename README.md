c0e117373615f0608d827cd5ff48224106225c61
caseynv <nwandukelechi@gmail.com>
2023-02-19 22:34:49 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-An extended work on Wikitask for Outreachy Round 25.
+Brief summary
+Wiki Loves Monuments is an international photographic competition held every year by the Wikimedia community. Since 2022, an interactive map application is being developed to improve user experience when participating in the competition, specifically with submitting their photographs to Wikimedia Commons, a free licensed media repository that feeds all Wikimedia projects, including Wikipedia.
+
+The app can serve as a base map app for the 30+ countries that hold local Wiki Loves Monuments and other geography-focused photographic competitions. The application is written in Python using a Flask framework, which facilitates collaborative development and can be forked if necessary, which gives more autonomy to the organizers.
+
+The application is intended to be responsive, bandwidth friendly and intuitive. Based on the feedback of consultants and participants, in this project you will use and expand Javascript and Python scripts in the Flask framework in order to create a responsive dashboard of the participant’s and the competition’s statistics within Wiki Loves Monuments editions, one of the main features suggested by the consultants. That feature aims to bring gamification elements to the competition in order to improve the user experience.
+
+This project is mentored by Éder Porto. SPARQL, Python and Javascript knowledge are advantage perks, but can be learnt during the project as well. Visual sense and product design knowledge and techniques will be useful, but are not essential.
+
+Note: An extended work on Wikitask for Outreachy Round 25.
 Database schema can be found here: https://dbdiagram.io/d/63b82a0c7d39e42284e95974
 
 Styling guide: https://design.wikimedia.org/style-guide/visual-style_colors.html
--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from datetime import date
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:\Users\NWANDU KELECHUKWU\Desktop\outreachy\code\Model\WLM.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:\Users\NWANDU KELECHUKWU\Desktop\outreachy\code\Model\wikilovesmonuments.db' 
 
 edition = db.Table('Edition', db.metadata, autoload=True, autoload_with=db.engine)
 photograph = db.Table('Photograph', db.metadata, autoload=True, autoload_with=db.engine)
@@ -28,23 +28,22 @@ monument = db.Table('Monument', db.metadata, autoload=True, autoload_with=db.eng
 @app.route('/')
 def dashboard():
     presentyear = date.today().year
-    puser = db.session.query(photograph).filter_by(edition_year=str(presentyear-1), person_id=1).count()
-    puser2015 = db.session.query(photograph).filter_by(edition_year=str(presentyear-8),person_id=1).count()
-    puser2016 = db.session.query(photograph).filter_by(edition_year=str(presentyear-7),person_id=1).count()
-    puser2018 = db.session.query(photograph).filter_by(edition_year=str(presentyear-5),person_id=1).count()
-    puser2019 = db.session.query(photograph).filter_by(edition_year=str(presentyear-4),person_id=1).count()
-    puser2020 = db.session.query(photograph).filter_by(edition_year=str(presentyear-3),person_id=1).count()
-    puser2021 = db.session.query(photograph).filter_by(edition_year=str(presentyear-2),person_id=1).count()
-    puser2022 = db.session.query(photograph).filter_by(edition_year=str(presentyear-1),person_id=1).count()
+    puser = db.session.query(photograph).filter_by(edition_year=str(presentyear-1), person_id=261).count()
+    puser2015 = db.session.query(photograph).filter_by(edition_year=str(presentyear-8),person_id=261).count()
+    puser2016 = db.session.query(photograph).filter_by(edition_year=str(presentyear-7),person_id=261).count()
+    puser2018 = db.session.query(photograph).filter_by(edition_year=str(presentyear-5),person_id=261).count()
+    puser2019 = db.session.query(photograph).filter_by(edition_year=str(presentyear-4),person_id=261).count()
+    puser2020 = db.session.query(photograph).filter_by(edition_year=str(presentyear-3),person_id=261).count()
+    puser2021 = db.session.query(photograph).filter_by(edition_year=str(presentyear-2),person_id=261).count()
+    puser2022 = db.session.query(photograph).filter_by(edition_year=str(presentyear-1),person_id=261).count()
     
-    data = db.session.query(photograph).filter_by(person_id=1).subquery()
+    data = db.session.query(photograph).filter_by(person_id=261).subquery()
     dataa = db.session.query(monument_photograph).join(data, data.c.monument_id == monument_photograph.c["photograph id"]).subquery()
     monumentcoords = db.session.query(monument.c.geographic_coordinates).join(dataa, monument.c.wikidata_qid == dataa.c["photograph id"]).all()
     monumentdetails = db.session.query(monument.c.image_filename).join(dataa, monument.c.wikidata_qid == dataa.c["photograph id"]).all()
-    photographcoords = db.session.query(photograph.c.camera_coordinates).filter_by(person_id=1).all()
-    photographdetails = db.session.query(photograph.c.filename).filter_by(person_id=1).all()
-    print(photographcoords)
-
+    photographcoords = db.session.query(photograph.c.camera_coordinates).filter_by(person_id=261).all()
+    photographdetails = db.session.query(photograph.c.filename).filter_by(person_id=261).all()
+    
     return render_template('index.html', photographdetails=photographdetails, photographcoordinate=photographcoords, monumentdetails=monumentdetails, monumentcoordinate=monumentcoords, user_cont=puser, puser2015=puser2015, puser2016=puser2016, puser2018=puser2018, puser2019=puser2019, puser2020=puser2020, puser2021=puser2021, puser2022=puser2022)
 
 @app.route('/stats')
@@ -60,9 +59,18 @@ def ranking():
     photograph2022 = db.session.query(photograph).filter_by(edition_year=str(presentyear-1)).count()
     
     editionyear = db.session.query(edition).filter_by(year=str(presentyear-1)).subquery()
-    dataa = db.session.query(photograph).join(editionyear, editionyear.c.place_1 == photograph.c.filename).all()
-    print(dataa)
-    return render_template('stats.html', edition2022=edition2022[0], photograph2015=photograph2015, photograph2016=photograph2016, photograph2018=photograph2018, photograph2019=photograph2019, photograph2020=photograph2020, photograph2021=photograph2021, photograph2022=photograph2022)
+    winner_1 = db.session.query(photograph).join(editionyear, editionyear.c.place_1 == photograph.c.filename).all()
+    winner_2 = db.session.query(photograph).join(editionyear, editionyear.c.place_2 == photograph.c.filename).all()
+    winner_3 = db.session.query(photograph).join(editionyear, editionyear.c.place_3 == photograph.c.filename).all()
+    winner_4 = db.session.query(photograph).join(editionyear, editionyear.c.place_4 == photograph.c.filename).all()
+    winner_5 = db.session.query(photograph).join(editionyear, editionyear.c.place_5 == photograph.c.filename).all()
+    winner_6 = db.session.query(photograph).join(editionyear, editionyear.c.place_6 == photograph.c.filename).all()
+    winner_7 = db.session.query(photograph).join(editionyear, editionyear.c.place_7 == photograph.c.filename).all()
+    winner_8 = db.session.query(photograph).join(editionyear, editionyear.c.place_8 == photograph.c.filename).all()
+    winner_9 = db.session.query(photograph).join(editionyear, editionyear.c.place_9 == photograph.c.filename).all()
+    winner_10 = db.session.query(photograph).join(editionyear, editionyear.c.place_10 == photograph.c.filename).all()
+    #print(winner_1)
+    return render_template('stats.html', edition2022=edition2022[0], photograph2015=photograph2015, photograph2016=photograph2016, photograph2018=photograph2018, photograph2019=photograph2019, photograph2020=photograph2020, photograph2021=photograph2021, photograph2022=photograph2022)#, winner_1=winner_1[0], winner_2=winner_2[0], winner_3=winner_3[0], winner_4=winner_4[0], winner_5=winner_5[0], winner_6=winner_6[0], winner_7=winner_7[0], winner_8=winner_8[0], winner_9=winner_9[0], winner_10=winner_10[0])
 
 
 @app.route('/stats/<year>')
@@ -70,9 +78,20 @@ def stats_year(year):
     editionyear = db.session.query(edition).filter_by(year=int(year)).all()
     photographyear = db.session.query(photograph).filter_by(edition_year=year).count()
     photographeryear = db.session.query(photograph).filter_by(edition_year=year).group_by(photograph.c.photographer).count()
-    
+    editionyearsub = db.session.query(edition).filter_by(year=year).subquery()
+    winner_1 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_1 == photograph.c.filename).all()
+    winner_2 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_2 == photograph.c.filename).all()
+    winner_3 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_3 == photograph.c.filename).all()
+    winner_4 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_4 == photograph.c.filename).all()
+    winner_5 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_5 == photograph.c.filename).all()
+    winner_6 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_6 == photograph.c.filename).all()
+    winner_7 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_7 == photograph.c.filename).all()
+    winner_8 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_8 == photograph.c.filename).all()
+    winner_9 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_9 == photograph.c.filename).all()
+    winner_10 = db.session.query(photograph).join(editionyearsub, editionyearsub.c.place_10 == photograph.c.filename).all()
+    print(winner_1)
     # show the user profile for that user
-    return render_template('statsyear.html', year=year, editionyear=editionyear[0], photographyear=photographyear, photographeryear=photographeryear)
+    return render_template('statsyear.html', year=year, editionyear=editionyear[0], photographyear=photographyear, photographeryear=photographeryear, winner_1=winner_1[0])#, winner_2=winner_2[0], winner_3=winner_3[0], winner_4=winner_4[0], winner_5=winner_5[0], winner_6=winner_6[0], winner_7=winner_7[0], winner_8=winner_8[0], winner_9=winner_9[0], winner_10=winner_10[0])
 
 # main driver function
 if __name__ == '__main__':
